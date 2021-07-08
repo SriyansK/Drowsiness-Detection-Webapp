fca585f507dec5a9f528d79bb1312ce235e91215
Sriyans <srkr.2805@gmail.com>
2021-07-08 11:10:37 +0530

    userDB added , new acc , login backend done

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
 const app = express();
 
@@ -19,7 +20,18 @@ const feedbackSchema = new mongoose.Schema({
     content : Array
 });
 
+const userSchema = new mongoose.Schema({
+    email : String,
+    nums: Array,
+    password: String,
+    status: Number
+});
+
+
 const Feedback = mongoose.model("Feedback",feedbackSchema);
+const User = mongoose.model("User",userSchema);
+
+var loginStatus=0;
 
 app.route("/")
     .get(function(req,res){
@@ -82,18 +94,91 @@ app.route("/login")
         res.render("login");
     })
     .post(function(req,res){
-        var username = req.body.username;
-        var password = req.body.password;
 
-        
-    })
+        if (loginStatus === 1){
+            console.log("Already Logged in.");
+            res.redirect("/");
+        }
+
+        var userEmail = req.body.username;
+        var userPassword = req.body.password;
+        User.findOne({email:userEmail},function(err,foundEmail){
+            if(err){
+                console.log(err);
+            }else{
+                if(foundEmail){
+                    bcrypt.hash(userPassword,saltRound,function(err,hash){
+                        bcrypt.compare(userPassword,foundEmail.password,function(err,result){
+                            if(err){
+                                console.log(err);
+                            }else{
+                                if(result === true){
+                                    foundEmail.status=1;
+                                    loginStatus=1;
+                                    foundEmail.save();
+                                    res.redirect("/");
+                                }else{
+                                    console.log("Wrong PassWord");
+                                }
+                            }
+                        });
+                    });
+                }else{
+                    console.log("User not found.");
+                    res.redirect("/create");
+                }
+            }
+        });
+    });
 
 app.route("/create")
     .get(function(req,res){
         res.render("create");
     })
     .post(function(req,res){
-        res.redirect("/");
+        
+        var userEmail = req.body.username;
+        var userPassword = req.body.password;
+        var userNew_password = req.body.new_password;
+        var userContact1 = req.body.phone;
+
+        User.findOne({email:userEmail},function(err,foundEmail){
+            if(err){
+                console.log(err);
+            }else{
+                if(foundEmail){
+                    console.log("Email already exist!");
+                }else{
+                    if(userPassword != userNew_password){
+                        console.log("Password Not matched");
+                    }else{
+                        bcrypt.hash(userPassword,saltRound,function(err,hash){
+
+                            if(err){
+                                console.log(err);
+                            }else{
+                                const newUser = new User({
+                                    email: userEmail,
+                                    nums: [userContact1],
+                                    password: hash,
+                                    status:0
+                                });
+        
+                                newUser.save(function(err){
+                                    if(err){
+                                        console.log("Error adding new user");
+                                    }else{
+                                        console.log("User added");
+                                    }
+                                });
+                            }     
+                        }); 
+                    }
+                }
+            }
+        });
+
+        res.redirect("/login");
     });
 
 app.listen(3000,function(req,res){
